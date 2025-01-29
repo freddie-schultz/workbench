850de8b673bf67402f983814206bc20aea5f1e28
Freddie Schultz <freddieschultz+devacademy@gmail.com>
2025-01-29 16:25:07 +1300

    Update complex number testing

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@ import * as eloquentJavaScript from '/js/eloquent-javascript.js'
 // import * as CN from '/js/complex-number.js'
 import { newCN } from '/js/complex-number.js'
 
-let test1 = newCN(2, 5)
-let test2 = newCN(4, -1)
+let test1 = newCN(-10, -5)
+let test2 = newCN(-2, -4)
 let test3 = test1.subtract(test2)
 let test4 = test1.multiply(test2)
 let test5 = test1.divide(test2)
 
-printComplexNumber(test1)
-printComplexNumber(test2)
-printComplexNumber(test3)
-printComplexNumber(test4)
-printComplexNumber(test5)
+console.log('cn1: ' + complexNumberText(test1))
+console.log('cn2: ' + complexNumberText(test2))
+console.log('cn1 - cn2: ' + complexNumberText(test3))
+console.log('cn1 * cn2: ' + complexNumberText(test4))
+console.log('cn1 / cn2: ' + complexNumberText(test5))
 
-function printComplexNumber(cn) {
+function complexNumberText(cn) {
   let cnText = `(${cn.real} `
   if (cn.complex < 0) {
     cnText += `- ${cn.complex * -1}i)`
   } else {
     cnText += `+ ${cn.complex}i)`
   }
-  console.log(cnText)
+  return cnText
 }
